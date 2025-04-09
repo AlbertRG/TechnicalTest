e3c9de55b6de1d149594eb7a875348869f1c9071
AlbertRG <alberto.rg94@hotmail.com>
2025-04-08 22:58:27 -0600

    fix issue where app was not compile successfully

--- app/src/main/java/com/coppel/technicaltest/MainActivity.kt
+++ app/src/main/java/com/coppel/technicaltest/MainActivity.kt
@@ -5,16 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.coppel.technicaltest.ui.navigation.NavigationWrapper
-import com.coppel.technicaltest.ui.theme.TechnicalTestTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TechnicalTestTheme {
-                NavigationWrapper()
-            }
+            NavigationWrapper()
         }
     }
 }
